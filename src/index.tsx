7d053180d3227753b4e8eaa70a1eeb7f134a27fd
とんさん <pinokio.tomo@gmail.com>
2025-08-13 10:21:30 +0900

    fix: drop stray Routers outside of entry (single-router rule)

--- src/index.tsx
+++ src/index.tsx
@@ -8,15 +8,11 @@ if (process.env.NODE_ENV === 'development') {
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-// import ErrorBoundary from './components/ErrorBoundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
