4e2b458df335acd6c43ffbbc26931625116c7a58
とんさん <pinokio.tomo@gmail.com>
2025-08-14 11:39:53 +0900

    chore(route): wire /daily-log to DailyLogPreview

---
+++ app/daily-log/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DailyLogPreview from "@/features/daily-log/DailyLogPreview";
+// 予備（@ が解決できない環境向けの相対パス案。コメントのまま残す）
+// import DailyLogPreview from "../../src/features/daily-log/DailyLogPreview";
+
+export default function Page() {
+  return <DailyLogPreview />;
+}
